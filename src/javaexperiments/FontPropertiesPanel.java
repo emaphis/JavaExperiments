6207336b60ee666cd121ed5376cf98678a71b3db
Ed Maphis <emaphis85@gmail.com>
2015-10-09 17:06:27 -0400

    Swing Stuff

---
+++ src/javaexperiments/FontPropertiesPanel.java
@@ -0,0 +1,125 @@
+// Just some experiments
+package javaexperiments;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * 
+ * @author emaphis
+ */
+public class FontPropertiesPanel extends JPanel {
+   protected JList<String> nameList;
+   protected JComboBox<Integer> sizeBox;
+   protected JCheckBox boldBox;
+   protected JCheckBox italicBox;
+   
+   protected SampleTextFrame frame;
+   
+   public final static int[] fontSizes = {10, 12, 14, 18, 24, 32, 48, 64};
+   
+   public FontPropertiesPanel(SampleTextFrame stf) {
+       super();
+       frame = stf;
+       createComponents();
+       buildLayot();
+   }
+
+    protected void buildLayot() {
+        JLabel label;
+        GridBagConstraints constraints = new GridBagConstraints();
+        GridBagLayout layout = new GridBagLayout();
+        setLayout(layout);
+        
+        constraints.insets = new Insets(5, 10, 5, 10);
+        constraints.gridx = 0;
+        label = new JLabel("Name:", JLabel.LEFT);
+        layout.setConstraints(label, constraints);
+        add(label);
+        label = new JLabel("Size:", JLabel.LEFT);
+        layout.setConstraints(label, constraints);
+        add(label);
+        layout.setConstraints(boldBox, constraints);
+        add(boldBox);
+        constraints.gridx++;
+        nameList.setVisibleRowCount(3);
+        JScrollPane jsp = new JScrollPane(nameList);
+        layout.setConstraints(jsp, constraints);
+        add(jsp);
+        layout.setConstraints(sizeBox, constraints);
+        add(sizeBox);
+        layout.setConstraints(italicBox, constraints);
+        add(italicBox);
+    }
+
+    protected void createComponents() {
+        GraphicsEnvironment ge =
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] names = ge.getAvailableFontFamilyNames();
+        nameList = new JList<>(names);
+        nameList.setSelectedIndex(0);
+        nameList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        nameList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent event) {
+                handleFontPropertyChange();       
+            }
+        }
+        );
+        
+        Integer sizes[] = new Integer[fontSizes.length];
+        for (int i = 0; i < sizes.length; i++) {
+            sizes[i] = fontSizes[i];
+        }
+        
+        sizeBox = new JComboBox<>(sizes);
+        sizeBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleFontPropertyChange();
+            }
+        }
+        );
+        
+        boldBox = new JCheckBox("Bold");
+        boldBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleFontPropertyChange();
+            }
+        }
+        );
+        
+        italicBox = new JCheckBox("Italic");
+        italicBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                handleFontPropertyChange();
+            }
+        }
+        );
+    }
+    
+    protected void handleFontPropertyChange() {
+        frame.refreshDisplayFont();
+    }
+    
+    public String getSelectedFontName() {
+        return nameList.getSelectedValue();
+    }
+    
+    public int getSelectedFontSize() {
+        return (Integer)sizeBox.getSelectedItem();
+    }
+    
+    public boolean isBoldSelected() {
+        return boldBox.isSelected();
+    }
+    
+    public boolean isItalicSelected() {
+        return italicBox.isSelected();
+    }
+}
---
+++ src/javaexperiments/SampleTextFrame.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexperiments;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+//import javax.swing.tree.DefaultTreeCellEditor;
+
+/**
+ *
+ * @author emaphis
+ */
+public class SampleTextFrame extends JFrame {
+    protected FontPropertiesPanel propertiesPanel;
+    protected JTextField sampleText;
+    protected JLabel displayArea;
+    
+    public static void main(String[] args) {
+        SampleTextFrame stf = new SampleTextFrame();
+        stf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        stf.setVisible(true);
+    }
+    
+    public SampleTextFrame() {
+        super();
+        createComponents();
+        createDocumentListener();
+        buildLayout();
+        refreshDisplayFont();
+        pack();
+    }
+
+    protected void createComponents() {
+        propertiesPanel = new FontPropertiesPanel(this);
+        sampleText = new JTextField(20);
+        displayArea = new JLabel("");
+        displayArea.setPreferredSize(new Dimension(200, 75));
+        displayArea.setMinimumSize(new Dimension(200, 75));
+    }
+
+    private void createDocumentListener() {
+        Document document = sampleText.getDocument();
+        document.addDocumentListener(new DocumentListener() {
+            @Override
+            public void changedUpdate(DocumentEvent event) {
+                handleDocumentUpdate();
+            }
+            
+            @Override
+            public void insertUpdate(DocumentEvent event) {
+                handleDocumentUpdate();
+            }
+            
+            @Override
+            public void removeUpdate(DocumentEvent event) {
+                handleDocumentUpdate();
+            }
+        }
+        );
+    }
+    
+    private void buildLayout() {
+        Container pane = getContentPane();
+        GridBagConstraints constraints = new GridBagConstraints();
+        GridBagLayout layout = new GridBagLayout();
+        pane.setLayout(layout);
+        constraints.insets = new Insets(5, 10, 5, 10);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1;
+        constraints.gridx = 0;
+        BevelBorder bb = new BevelBorder(BevelBorder.RAISED);
+        TitledBorder tb = new TitledBorder(bb, "Font");
+        propertiesPanel.setBorder(tb);
+        layout.setConstraints(propertiesPanel, constraints);
+        pane.add(propertiesPanel);
+        layout.setConstraints(sampleText, constraints);
+        pane.add(sampleText);
+        layout.setConstraints(displayArea, constraints);
+        pane.add(displayArea);
+    }
+    
+    protected void handleDocumentUpdate() {
+        displayArea.setText(sampleText.getText());
+    }
+
+    protected void refreshDisplayFont() {
+        displayArea.setFont(getSelectedFont());
+    }
+    
+    public Font getSelectedFont() {
+        String name = propertiesPanel.getSelectedFontName();
+        int style = 0;
+        style += (propertiesPanel.isBoldSelected() ? Font.BOLD : 0);
+        style += (propertiesPanel.isItalicSelected() ? Font.ITALIC : 0);
+        int size = propertiesPanel.getSelectedFontSize();
+        return new Font(name, style, size);
+    }
+}
