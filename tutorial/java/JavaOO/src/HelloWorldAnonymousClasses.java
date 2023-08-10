d54c501ea7c5095a4f6c8e1e185ed2bf342eb309
emaphis <emaphis85@gmail.com>
2023-08-10 13:25:54 -0400

    Nested and inner classes

---
+++ tutorial/java/JavaOO/src/HelloWorldAnonymousClasses.java
@@ -0,0 +1,75 @@
+
+/**
+ * Uses anonymous classes in the initialization statements of the local variables
+ * frenchGreeting and spanishGreeting, but uses a local class for the
+ * initialization of the variable englishGreeting:
+ * @author emaph
+ */
+public class HelloWorldAnonymousClasses {
+
+    interface HelloWorld {
+        public void greet();
+        public void greetSomeone(String someone);
+    }
+
+    public void sayHello() {
+
+        // regular inner class
+        class EnglishGreeting implements HelloWorld {
+            String name = "world";
+
+            @Override
+            public void greet() {
+                greetSomeone("world");
+            }
+
+            @Override
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hello " + name);
+            }
+        }
+
+        HelloWorld englishGreeting = new EnglishGreeting();
+
+        // anonymous class
+        HelloWorld frenchGreeting = new HelloWorld() {
+            String name = "tout le monde";
+
+            @Override
+            public void greet() {
+                greetSomeone(name);
+            }
+
+            @Override
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Salute " + name);
+            }
+        };
+
+        HelloWorld spanishGreeting = new HelloWorld() {
+            String name = "mundo";
+
+            @Override
+            public void greet() {
+                greetSomeone("mundo");
+            }
+
+            @Override
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hola " + name);
+            }
+        };
+
+        englishGreeting.greet();
+        frenchGreeting.greet();
+        spanishGreeting.greet();
+    }
+
+    public static void main(String[] args) {
+        HelloWorldAnonymousClasses myApp = new HelloWorldAnonymousClasses();
+        myApp.sayHello();
+    }
+}
