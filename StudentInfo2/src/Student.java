be992b9d3e50e464ebb6d87b8ee4e7c3fce24e78
emaphis <emaphis85@gmail.com>
2025-04-13 17:27:46 -0400

    Add a basic Student class with a name field. Add a test class for Student. Test name handling.

---
+++ StudentInfo2/src/Student.java
@@ -0,0 +1,17 @@
+
+/**
+ * Represents an individual Student in the system.
+ * @author emaph
+ */
+class Student {
+    private final String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+}
---
+++ StudentInfo2/test/StudentTest.java
@@ -0,0 +1,23 @@
+
+import junit.framework.TestCase;
+
+/**
+ * Test cases for Student class.
+ * @author emaph
+ */
+public class StudentTest extends TestCase {
+
+    public void testCreate() {
+        final String firstStudentName = "Jane Doe";
+        Student firstStudent = new Student(firstStudentName);
+        assertEquals(firstStudentName, firstStudent.getName());
+
+        final String secondStudentName = "Joe Blow";
+        Student secondStudent = new Student(secondStudentName);
+        assertEquals(secondStudentName, secondStudent.getName());
+
+        assertEquals(firstStudentName, firstStudent.getName());
+    }
+
+
+}
