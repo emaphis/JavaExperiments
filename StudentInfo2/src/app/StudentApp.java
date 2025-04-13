e91bcb619dc94a05811c0994b7737b071a4b80ec
emaphis <emaphis85@gmail.com>
2025-04-13 12:05:08 -0400

    New unit testing project.

---
+++ StudentInfo2/src/app/StudentApp.java
@@ -0,0 +1,19 @@
+
+
+package app;
+
+/**
+ * This Student Application
+ * @author emaph
+ */
+public class StudentApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Processing Students ...");
+        System.out.println("Done!");
+    }
+
+}
