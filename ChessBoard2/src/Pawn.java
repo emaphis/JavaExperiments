b113a66bad01b5a56dccb4a8c83604e457f1dd7b
emaphis <emaphis85@gmail.com>
2025-04-15 15:15:58 -0400

    Move color constants to Pawn class, make them static class constants. Create a default Pawn constuctor that creates White Pawns.
    Signed-off-by: emaphis <emaphis85@gmail.com>

--- ChessBoard2/src/Pawn.java
+++ ChessBoard2/src/Pawn.java
@@ -5,10 +5,17 @@
  * @author emaph
  */
 class Pawn {
+    static final String WHITE = "white";
+    static final String BLACK = "black";
+
     private final String color;
 
     Pawn(String color) {
         this.color = color;
+    }
+
+    Pawn() {
+        this.color = Pawn.WHITE;
     }
 
     String getColor() {
--- ChessBoard2/test/PawnTest.java
+++ ChessBoard2/test/PawnTest.java
@@ -8,15 +8,13 @@ import junit.framework.TestCase;
 public class PawnTest extends TestCase {
 
     public void testCreate() {
-        final String firstColor = "white";
-        Pawn firstPawn = new Pawn(firstColor);
-        assertEquals(firstColor, firstPawn.getColor());
+        Pawn firstPawn = new Pawn();
+        assertEquals(Pawn.WHITE, firstPawn.getColor());
 
-        final String secondColor = "black";
-        Pawn secondPawn = new Pawn(secondColor);
-        assertEquals(secondColor, secondPawn.getColor());
+        Pawn secondPawn = new Pawn(Pawn.BLACK);
+        assertEquals(Pawn.BLACK, secondPawn.getColor());
 
-        assertEquals(firstColor, firstPawn.getColor());
+        assertEquals(Pawn.WHITE, firstPawn.getColor());
     }
 
 }
