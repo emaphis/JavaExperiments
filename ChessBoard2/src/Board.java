f04f254178c85fc8e7ee0f117c79b74fb9c2fb1e
emaphis <emaphis85@gmail.com>
2025-04-15 17:50:06 -0400

    Add an ArrayList to Board to store Pawns. Add methods to add Pawns and get Pawn at index.

--- ChessBoard2/src/Board.java
+++ ChessBoard2/src/Board.java
@@ -1,12 +1,24 @@
 
+import java.util.ArrayList;
+
+
 /**
  * Represents a chess Board
  * @author emaph
  */
 class Board {
+    private ArrayList<Pawn> pieces = new ArrayList<Pawn>();
 
     int getNumberPieces() {
-        return 0;
+        return pieces.size();
+    }
+
+    void addPiece(Pawn pawn) {
+        pieces.add(pawn);
+    }
+
+    Pawn getPiece(int index) {
+        return pieces.get(index);
     }
 
 }
--- ChessBoard2/test/BoardTest.java
+++ ChessBoard2/test/BoardTest.java
@@ -12,4 +12,18 @@ public class BoardTest extends TestCase {
         Board board = new Board();
         assertEquals(0, board.getNumberPieces());
     }
+
+    public void testAddPiece() {
+        Board board = new Board();
+
+        Pawn pawn1 = new Pawn();  // white
+        board.addPiece(pawn1);
+        assertEquals(1, board.getNumberPieces());
+        assertEquals(Pawn.WHITE, board.getPiece(0).getColor());
+
+        Pawn pawn2 = new Pawn(Pawn.BLACK);
+        board.addPiece(pawn2);
+        assertEquals(2, board.getNumberPieces());
+        assertEquals(Pawn.BLACK, board.getPiece(1).getColor());
+    }
 }
