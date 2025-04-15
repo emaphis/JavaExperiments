2911c7ad0e15be4487c3e165c2cf3cf3794658c7
emaphis <emaphis85@gmail.com>
2025-04-15 17:55:09 -0400

    Add Javadoc to Pawn and Board classes

--- ChessBoard2/src/Board.java
+++ ChessBoard2/src/Board.java
@@ -9,14 +9,25 @@ import java.util.ArrayList;
 class Board {
     private ArrayList<Pawn> pieces = new ArrayList<Pawn>();
 
-    int getNumberPieces() {
-        return pieces.size();
-    }
-
+    /**
+     * Add passed Pawn to Board.
+     * @param pawn add to board
+     */
     void addPiece(Pawn pawn) {
         pieces.add(pawn);
     }
 
+    /**
+     * @return number of Pawn contained by Board
+     */
+    int getNumberPieces() {
+        return pieces.size();
+    }
+
+    /**
+     * @param index of Pawn
+     * @return Pawn at index.
+     */
     Pawn getPiece(int index) {
         return pieces.get(index);
     }
--- ChessBoard2/src/Pawn.java
+++ ChessBoard2/src/Pawn.java
@@ -10,14 +10,24 @@ class Pawn {
 
     private final String color;
 
+    /**
+     * Create a Pawn of a given color.
+     * @param color
+     */
     Pawn(String color) {
         this.color = color;
     }
 
+    /**
+     * Create a default (white) Pawn
+     */
     Pawn() {
         this.color = Pawn.WHITE;
     }
 
+    /**
+     * @return Pawn's color
+     */
     String getColor() {
         return color;
     }
