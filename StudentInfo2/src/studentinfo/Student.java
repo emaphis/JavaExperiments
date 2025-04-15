94540fa548da250decafbbcba8d914965b280bef
emaphis <emaphis85@gmail.com>
2025-04-15 15:06:07 -0400

    Provide Javadoc for Student class.

--- StudentInfo2/src/studentinfo/Student.java
+++ StudentInfo2/src/studentinfo/Student.java
@@ -8,10 +8,17 @@ package studentinfo;
 class Student {
     private final String name;
 
+    /**
+     * Create a Student with given name
+     * @param name of Student
+     */
     Student(String name) {
         this.name = name;
     }
 
+    /**
+     * @return Student name
+     */
     String getName() {
         return name;
     }
