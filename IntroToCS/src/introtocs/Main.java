e7b81ae0d0448651261ef41c4d78563120cf8847
emaphis <emaphis85@gmail.com>
2025-05-11 22:10:58 -0400

    Add IntroToCS project

---
+++ IntroToCS/src/introtocs/Main.java
@@ -0,0 +1,17 @@
+
+package introtocs;
+
+/**
+ * Intro to CS
+ * @author emaph
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Into to CS.");
+    }
+
+}
