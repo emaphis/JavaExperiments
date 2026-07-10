6de92b02bd8b4a8b8cd0f7e7bd11f81d5318567b
Ed <emaphis85@gmail.com>
2026-07-10 11:58:12 -0400

    ChessBoard Create a testsuite Refactor ChessBoardTest, move item createm to setUp()

---
+++ ChessBoard2/test/chess/AllTests.java
@@ -0,0 +1,18 @@
+
+import studentinfo.StudentTest;
+import studentinfo.CourseSessionTest;
+import junit.framework.TestSuite;
+
+/**
+ *
+ * @author emaph
+ */
+public class AllTests {
+    public static TestSuite suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTestSuite(StudentTest.class );
+        suite.addTestSuite(CourseSessionTest.class);
+        return suite;
+    }
+    
+}
